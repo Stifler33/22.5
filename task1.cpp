31c607231bba58be5c3a783a8f0ba13c06f1363e
stifler33 <bagi33.ru@gmail.com>
2021-10-08 22:43:52 +0300

    task1 fixed

--- task1.cpp
+++ task1.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <vector>
 using namespace std;
-string family(string &operation){
+string getFamily(string &operation){
     string input;
     for (int i = 0; i < operation.length(); i++){
         if (operation[i] >= 'a' && operation[i] <= 'z'){
@@ -11,36 +12,65 @@ string family(string &operation){
     }
     return input;
 }
+string getNumber(string &operation){
+    string input;
+    for (int i = 0; i < operation.length(); i++){
+        if (operation[i] >= '0' && operation[i] <= '9'){
+            input += operation[i];
+        }
+    }
+    return input;
+}
 int main() {
-    map<int, string> telephone;
+    //  number  family
+    map<string, string> numberToFamily;
+    //  family  number
+    map<string, vector<string>> familyToNumber;
+    vector<string> vecNumber;
     //init telephone book
-    telephone.insert(make_pair<int, string>(748596, "pavlov"));
-    telephone.insert(make_pair<int, string>(331525, "ivanov"));
-    telephone.insert(make_pair<int, string>(551241, "pavlov"));
-    telephone.insert(make_pair<int, string>(361655, "sidorov"));
-
     string operation;
     cout << "Enter operation\n";
     while(operation != "exit") {
         getline(cin, operation);
         if (operation == "exit") continue;
-
-        if (operation[0] >= 'a' && operation[0] <= 'z') {
-            for (map<int, string>::iterator searchFamily = telephone.begin(); searchFamily != telephone.end(); ++searchFamily){
-                if (searchFamily->second == operation) cout << searchFamily->first << " ";
-            }
-            cout << endl;
-        } else if (operation[0] >= '0' && operation[0] <= '9') {
-            if (operation.back() >= 'a' && operation.back() <= 'z') {
-                telephone.insert(pair<int, string>(stoi(operation), family(operation)));
-            } else {
-                for (map<int, string>::iterator numb = telephone.begin(); numb != telephone.end(); ++numb) {
-                    if (stoi(operation) == numb->first) {
-                        cout << numb->second << endl;
-                    }
+        //list book
+        if (operation == "list"){
+            cout << "family to number\n";
+            for (auto outFN = familyToNumber.begin(); outFN != familyToNumber.end(); ++outFN){
+                cout << outFN->first;
+                for (int i = 0; i < outFN->second.size(); i++){
+                    cout << outFN->second[i] << " ";
                 }
+                cout << endl;
+            }
+            cout << "number to family\n";
+            for (auto outNF = numberToFamily.begin(); outNF != numberToFamily.end(); ++outNF){
+                cout << outNF->first << " " << outNF->second << endl;
             }
         }
-    }
+        // get number
+        if (operation[0] >= 'a' && operation[0] <= 'z') {
+
+            // get family or added number to book
+        } else if (operation[0] >= '0' && operation[0] <= '9') {
+           //added number book
+           if (operation.back() >= 'a' && operation.back() <= 'z'){
+               string number = getNumber(operation);
+               string family = getFamily(operation);
+               if (numberToFamily.count(number.c_str()) == 0) {
+                   numberToFamily.insert(make_pair(number, family));
+                   map<string, vector<string>>::iterator vecMapNumb = familyToNumber.begin();
+                   if (familyToNumber.count(family.c_str()) == 0){
+                       familyToNumber.insert(make_pair(family, vecNumber));
+                        vecMapNumb->second.push_back(number);
+                   }else {
+                       familyToNumber[family].push_back(number);
+                   }
+               }
+           }
+            } else {
+
+                }
+            }
     return 0;
 }
